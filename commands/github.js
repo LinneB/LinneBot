b6895bd3f3098914b2088af47b6a5b5fa01e5570
LinneB <77924330+LinneB@users.noreply.github.com>
2023-12-08 16:25:50 +0100

    feat(command): Add github command

---
+++ commands/github.js
@@ -0,0 +1,12 @@
+module.exports = {
+  name: "github",
+  aliases: ["#github"],
+  cooldown: 1000,
+  help: "Sends the link to the LinneBot GitHub repository",
+  usage: "#github",
+  run: function() {
+    return {
+      reply: "https://github.com/LinneB/LinneBot"
+    };
+  }
+};
