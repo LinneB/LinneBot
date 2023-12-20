eee7024b8e52387494148221ec4087a05dcedb83
LinneB <77924330+LinneB@users.noreply.github.com>
2023-12-20 19:45:34 +0100

    refactor(index.js): Clean up initialization

--- index.js
+++ index.js
@@ -4,20 +4,19 @@ const { getConfig } = require("./misc/config");
 const tmiClient = require("./providers/irc");
 const { onChat, onReady } = require("./misc/handler");
 
-require("./web");
-require("./misc/commands");
+log("info", "Connecting to chat...");
+tmiClient.on("PRIVMSG", onChat);
+tmiClient.on("ready", onReady);
+tmiClient.connect();
+const channels = getConfig("channels");
+log("info", `Joining ${channels.length} ${channels.length > 1 ? "chats" : "chat"}`);
+tmiClient.joinAll(getConfig("channels"));
 
-(async () => {
-  log("info", "Connecting to chat...");
-  tmiClient.on("PRIVMSG", onChat);
-  tmiClient.on("ready", onReady);
-  tmiClient.connect();
-  tmiClient.joinAll(getConfig("channels"));
-  log("info", "Initializing EventSub...");
-  require("./providers/eventsub");
-  log("info", "Initializing database...");
-  require("./providers/mongodb");
-})();
+log("info", "Initializing EventSub...");
+require("./providers/eventsub");
+
+log("info", "Initializing database...");
+require("./providers/mongodb");
 
 // This signal handler is required for Node to shut down properly when running in Docker
 process.on("SIGTERM", () => {
