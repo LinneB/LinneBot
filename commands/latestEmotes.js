19cd952d6d38ae69d80654a6f511731b00e04ae1
LinneB <77924330+LinneB@users.noreply.github.com>
2024-02-23 12:23:57 +0100

    feat(command): Include added by username in 7TV commands

--- commands/latestEmotes.js
+++ commands/latestEmotes.js
@@ -19,18 +19,45 @@ module.exports = {
         };
       }
     }
-    const emotes = await seventv.getEmoteSet(userid);
+    const emotes = await seventv
+      .getEmoteSet(userid)
+      .then((emotes) => emotes.slice(-5));
     if (emotes.length === 0) {
       return {
         reply: "This channel does not have any 7tv emotes",
       };
     }
-    const latestEmotes = emotes.slice(-5).map((emote) => {
+
+    const actors = new Set();
+    emotes.map((emote) => actors.add(emote.actor_id));
+
+    const requests = [...actors].map((actor) => {
+      return seventv.axios({
+        method: "get",
+        url: `/users/${actor}`,
+      });
+    });
+    const sevenTVIDs = await Promise.all(requests).then((responses) => {
+      const output = {};
+      for (const res of responses) {
+        output[res.data.id] = res.data.display_name;
+      }
+      return output;
+    });
+
+    const latestEmotes = emotes.map((emote) => {
       const timeAgo = utils.formattedTimeAgoString(
         Date.now() - new Date(emote.timestamp),
       );
+      const actor = sevenTVIDs[emote.actor_id];
+      if (actor) {
+        return `${emote.name} (added by ${actor} ${
+          timeAgo ? `${timeAgo} ago` : "just now"
+        })`;
+      }
       return `${emote.name} (${timeAgo ? `${timeAgo} ago` : "just now"})`;
     });
+
     return {
       reply: latestEmotes.reverse().join(", "),
     };
